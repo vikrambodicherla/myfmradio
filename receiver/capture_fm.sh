604137f66c89ba72bcba7096d41454b664ee43f4
Vikram Bodicherla <vikrambd@yahoo-inc.com>
2016-11-26 23:21:28 -0500

    First commit - receiver script

---
+++ receiver/capture_fm.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#rtl_fm -M wbfm -f 88.3M | ffmpeg -f s16le -ar 17000 -ac 2 -i - wbfm2.mp3
+
+FREQ=$1
+CLIP_LENGTH=$2
+OUTPUT=$3
+
+cd $OUTPUT
+
+while : 
+do
+	TEMP_FILENAME=_`date +%s`.mp3
+
+	START_TIME=`date +%s`
+
+	# Execute the job
+	echo "Capturing" $FREQ "for" $CLIP_LENGTH "seconds..."
+
+    rtl_fm -M wbfm -f $FREQ | ffmpeg -f s16le -ar 16000 -ac 2 -i - $TEMP_FILENAME &
+
+	# Get its PID
+	PID=$!
+
+	# Sleep
+	sleep $CLIP_LENGTH
+
+	# SIGTERM
+	kill $PID
+
+	# Save file
+	END_TIME=`date +%s`
+	FILENAME=$START_TIME,$(($END_TIME-$START_TIME)).mp3
+	
+	echo "Saving to" $FILENAME
+	mv $TEMP_FILENAME $FILENAME
+
+	sleep 1
+done
